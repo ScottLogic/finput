0ac6e035331d194931f360fcf70b23f73c3077d1
Ryan Grey <rgrey@scottlogic.co.uk>
2017-07-07 16:03:31 +0100

    test: Switching delimiters

--- test/pageObjects/index.js
+++ test/pageObjects/index.js
@@ -4,10 +4,14 @@ import { driver, defaultTimeout } from '../helpers';
 const rootSelector = { css: '#root' };
 const finputDefaultSelector = { css: '#finput-default' };
 const finputReversedDelimitersSelector = { css: '#finput-reversed-delimiters' };
+const finputSwitchOptionsSelector = { css: '#finput-switch-options' }; 
+const finputSwitchOptionsButtonSelector = { css: '#finput-switch-options-button' }; 
 const nativeTextSelector = { css: '#native-text' };
 
 export const finputDefault = () => driver.findElement(finputDefaultSelector);
 export const finputReversedDelimiters = () => driver.findElement(finputReversedDelimitersSelector);
+export const finputSwitchOptions = () => driver.findElement(finputSwitchOptionsSelector);
+export const finputSwitchOptionsButton = () => driver.findElement(finputSwitchOptionsButtonSelector);
 export const nativeText = () => driver.findElement(nativeTextSelector);
 
 const root = () => driver.findElement(rootSelector);
