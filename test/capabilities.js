0a9596f8027794aff047ab1bd7f6a5a329ab8cf8
Dean Kerr <dean_k@live.co.uk>
2017-08-06 17:48:58 +0100

    chore: add browserstack specific capabilities for browser version and OS version

--- test/capabilities.js
+++ test/capabilities.js
@@ -6,10 +6,15 @@ const capabilities = {
   [Capability.BROWSER_NAME]: Browser.CHROME
 };
 
+// browserstack specific capabilities - https://www.browserstack.com/automate/capabilities
 if (process.env.CI) {
   capabilities['browserstack.local'] = true;
   capabilities['browserstack.localIdentifier'] = process.env.BROWSERSTACK_LOCAL_IDENTIFIER || process.env.TRAVIS_JOB_NUMBER;
   capabilities['browserstack.networkLogs'] = true;
+  capabilities.os = 'WINDOWS';
+  capabilities.os_version = '10';
+  capabilities.browser = Browser.CHROME;
+  capabilities.browser_verison = '60';
 }
 
 export default capabilities;
--- test/setupTests.js
+++
@@ -1,2 +0,0 @@
-process.env.USE_PROMISE_MANAGER = false;
-jasmine.DEFAULT_TIMEOUT_INTERVAL = 60e3;
