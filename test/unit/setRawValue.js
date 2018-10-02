1700f25c5ecff7867dec2a8b655714478440e908
Martin Moxon <moxon6@googlemail.com>
2018-10-02 14:56:50 +0100

    Added test for exsting empty behaviour

--- test/unit/setRawValue.js
+++ test/unit/setRawValue.js
@@ -19,4 +19,17 @@ describe('setRawValue', () => {
         expect(element.value).toBe('0.00');
     });
 
+    it('has an initial value of empty string and rawValue of undefined', () => {
+        expect(element.value).toBe('');
+        expect(element.rawValue).toBe(undefined);
+    });
+
+    it('sets value to empty string and rawValue to 0 when entry is deleted', () => {
+        element.setRawValue(100);
+        element.setRawValue('');
+
+        expect(element.value).toBe('');
+        expect(element.rawValue).toBe(0);
+    });
+
 });
