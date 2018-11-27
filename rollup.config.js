975729e5056507239e5e44e5ffebf07a590d22f3
Dean Kerr <dkerr@scottlogic.co.uk>
2018-11-27 10:19:15 +0000

    feature: add rollup configuration

---
+++ rollup.config.js
@@ -0,0 +1,22 @@
+import commonjs from 'rollup-plugin-commonjs';
+
+import resolve from 'rollup-plugin-node-resolve';
+import typescript from 'rollup-plugin-typescript2'
+
+import pkg from './package.json'
+
+export default {
+    input: 'src/finput.ts',
+    output: {
+        file: pkg.unpkg,
+        format: 'iife',
+        name: 'finput'
+    },
+    plugins: [
+        commonjs(),
+        resolve(),
+        typescript({
+            typescript: require('typescript'),
+        }),
+    ],
+}
