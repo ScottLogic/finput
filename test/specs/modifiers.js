73f58367f7a8ad227ca8b7b64ac19760cd2ec6f7
Dean Kerr <dkerr@scottlogic.co.uk>
2018-11-29 13:03:29 +0000

    feature: update keyboard shortcut modifier tests

--- test/specs/modifiers.js
+++ test/specs/modifiers.js
@@ -8,21 +8,18 @@ describe('modifiers', () => {
 
   describe('are not blocked from activating keyboard shortcuts', () => {
     // TODO: find a way to test browser shortcuts blur the input (not currently working with chromedriver)
-    typing('k').whileModifierPressed().shouldShow('');
-    typing('m').whileModifierPressed().shouldShow('');
-    typing('b').whileModifierPressed().shouldShow('');
-    typing('l').whileModifierPressed().shouldShow('');
-    typing('h').whileModifierPressed().shouldShow('');
+    typing('k').whileModifierPressed().shouldShow('1,000');
+    typing('m').whileModifierPressed().shouldShow('1,000,000');
+    typing('b').whileModifierPressed().shouldShow('1,000,000,000');
 
-    typing('0').whileModifierPressed().shouldShow('');
-    typing('1').whileModifierPressed().shouldShow('');
-    typing('2').whileModifierPressed().shouldShow('');
-    typing('3').whileModifierPressed().shouldShow('');
+    typing('0').whileModifierPressed().shouldShow('0');
+    typing('1').whileModifierPressed().shouldShow('1');
+    typing('2').whileModifierPressed().shouldShow('2');
+    typing('3').whileModifierPressed().shouldShow('3');
 
-    typing('-').whileModifierPressed().shouldShow('');
+    typing('-').whileModifierPressed().shouldShow('-');
     typing('+').whileModifierPressed().shouldShow('');
-    typing('.').whileModifierPressed().shouldShow('');
+    typing('.').whileModifierPressed().shouldShow('.');
     typing(',').whileModifierPressed().shouldShow('');
   });
 });
-;
